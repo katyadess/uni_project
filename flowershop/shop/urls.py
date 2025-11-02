cdf6d4f460d3e30b5a7750af2f1aeae1fe97a43c
katyadess <trynovakatya345@gmail.com>
2025-11-02 18:53:20 +0200

    create info views and urls

--- flowershop/shop/urls.py
+++ flowershop/shop/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('main/', views.main, name='main'),
     path('registration/', views.registration, name='registration'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('delivery/', views.delivery, name='delivery'),
+    path('payment/', views.payment, name='payment'),
     path('flower/', views.flower_detail, name='flower_detail'),
 ]
--- flowershop/shop/views.py
+++ flowershop/shop/views.py
@@ -5,10 +5,19 @@ def home(request):
     return render(request, 'shop/home.html')
 
 def main(request):
-    return render(request, 'shop/main2.html')
+    return render(request, 'shop/main.html')
 
 def registration(request):
     return render(request, 'shop/registration.html')
+
+def about_us(request):
+    return render(request, 'shop/ProNas_info.html')
+
+def delivery(request):
+    return render(request, 'shop/dostavka_info.html')
+
+def payment(request):
+    return render(request, 'shop/oplata_info.html')
 
 def flower_detail(request):
     return render(request, 'shop/pro_tovar.html')
